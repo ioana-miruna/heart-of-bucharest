eb2afa6ac416a4c65629314db6867481a68c8fa5
Ioana Badica <ioanabadica@Ioanas-MacBook-Air.local>
2025-04-29 10:35:43 +0300

    Initialize Spring Boot project

---
+++ heart-of-bucharest/src/main/java/com/example/heartofbucharest/HeartOfBucharestApplication.java
@@ -0,0 +1,13 @@
+package com.example.heartofbucharest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartOfBucharestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeartOfBucharestApplication.class, args);
+    }
+
+}
